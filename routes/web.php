a403fa9422288e3b0ed3341c8f2ad44077cf0fe8
XimenZangief <s9671073@gmail.com>
2022-07-07 08:34:07 +0800

    套用內建的登入介面 composer require laravel/ui php artisan ui vue --auth 載入bootstraps 4

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{name}', function ($name) {
-    return view('welcome02')->with('name',$name);
-});
-// Route::get('/', function () {
-//     return view('welcome');
+// Route::get('/{name}', function ($name) {
+//     return view('welcome02')->with('name',$name);
 // });
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
