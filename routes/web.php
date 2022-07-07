e4ef229281b0b780a5a04e0040f2f9ea82ca02a9
XimenZangief <s9671073@gmail.com>
2022-07-07 08:20:23 +0800

    新增Route導向的view頁面：welcome02 使用RESTful get動態顯示name的歡迎詞

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{name}', function ($name) {
+    return view('welcome02')->with('name',$name);
 });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
